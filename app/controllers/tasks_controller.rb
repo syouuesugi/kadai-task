494826d9f0a0abc28594aa631e5e5b174d19f62a
syou.uesugi <0py7022t2p.552z@ezweb.ne.jp>
2021-05-01 08:29:32 +0000

    resubmit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,10 +15,10 @@ class TasksController < ApplicationController
     @task = Task.new(task_params)
 
     if @task.save
-      flash[:success] = 'Tasks が正常に投稿されました'
+      flash[:success] = 'タスク が正常に投稿されました'
       redirect_to @task
     else
-      flash.now[:danger] = 'Tasks が投稿されませんでした'
+      flash.now[:danger] = 'タスク が投稿されませんでした'
       render :new
     end
   end
@@ -31,10 +31,10 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
 
     if @task.update(task_params)
-      flash[:success] = 'Task は正常に更新されました'
+      flash[:success] = 'タスクは正常に更新されました'
       redirect_to @task
     else
-      flash.now[:danger] = 'Task は更新されませんでした'
+      flash.now[:danger] = 'タスク は更新されませんでした'
       render :edit
     end
   end
@@ -43,7 +43,7 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
     @task.destroy
 
-    flash[:success] = 'Task は正常に削除されました'
+    flash[:success] = 'タスク は正常に削除されました'
     redirect_to tasks_url
   end
   
