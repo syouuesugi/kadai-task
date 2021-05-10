30cca5e77362f3e64856dffecc9da46e1c5a7421
syou.uesugi <0py7022t2p.552z@ezweb.ne.jp>
2021-05-10 11:33:50 +0000

    for heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   def index
-    @tasks=Task.all
+    @tasks=current_user.tasks.order(id: :desc).page(params[:page])
   end
 
   def show
@@ -12,12 +13,13 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'タスク が正常に投稿されました'
       redirect_to @task
     else
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'タスク が投稿されませんでした'
       render :new
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,27 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.order(id: :desc).page(params[:page]).per(25)
-  end
-
+  before_action :require_user_logged_in, only: [:edit, :show]
+  
   def show
     @user = User.find(params[:id])
+    @tasks = @user.tasks.order(id: :desc).page(params[:page])
+    counts(@user)
   end
-
+  
+  def edit 
+    @user=User.find(params[:id])
+  end 
+  
+  def update
+    @user=User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "プロフィールを変更しました"
+      redirect_to tasks_path
+    else 
+      flash[:danger] = "プロフィールを変更できませんでした。"
+      render :edit
+    end 
+  end
+  
   def new
     @user = User.new
   end
@@ -16,7 +31,7 @@ class UsersController < ApplicationController
     
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to @user
+      redirect_to login_path
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
